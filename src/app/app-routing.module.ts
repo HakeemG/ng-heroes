cb546347a30a2f189442216f043c7ea7f965f6ac
Hakeem Giydan <hgiydan@gmail.com>
2019-08-02 09:38:08 -0700

    Routing
    Signed-off-by: Hakeem Giydan <hgiydan@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Dashboard', pathMatch: 'full'},
+  { path: 'Dashboard', component: DashboardComponent },
+  { path: 'Details/:id', component: HeroDetailsComponent },
+  { path: 'Heroes', component: HeroesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
